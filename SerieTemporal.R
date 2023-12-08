0c00cf8788955341be5eed12ab25ac9f87c300a6
Igor Luna <igorlunad@gmail.com>
2023-12-08 13:48:32 -0300

    Primeiros modelos de serie temporal e tratamento dos dados para XTS

--- SerieTemporal.R
+++ SerieTemporal.R
@@ -25,5 +25,69 @@ df <- read_delim("C:/Users/igor-/OneDrive/Área de Trabalho/Deepen/NEXXERA/DOCS_
 
 
 
-#Trabalhar de forma semanal
+#Trabalhar de forma semanal x = df[df['COD_CEDENTE'] == 38631]
+
+x<-df %>% dplyr::filter(COD_CEDENTE == '38631')
+
+
+x <- x %>% dplyr::select(c('DATA_FORMATADA','TOTAL'))
+
+
+
+# ACF e PACF --------------------------------------------------------------
+
+ret <- x$TOTAL[-1]
+ret2 <- x$TOTAL[-1]^2
+
+Acf(ret,60, na.action = na.pass)
+Acf(x$TOTAL,60, na.action = na.pass)
+Acf(ret2,60, na.action = na.pass)
+Acf(ret2,60,type="partial", na.action = na.pass)
+
+
+# Time series -------------------------------------------------------------
+
+
+time_series_weekly <- ts(x$TOTAL, frequency = 52)  # 52 weeks in a year
+
+# If you're using 'xts' for date-based data
+data_xts_weekly <- xts(x$TOTAL, order.by = as.Date(x$DATA_FORMATADA))
+
+
+plot(time_series_weekly)
+
+
+decomposed_weekly <- decompose(time_series_weekly)
+autoplot(decomposed_weekly)
+
+
+
+
+# Forecast básico ---------------------------------------------------------
+
+
+
+# Fit a time series model (e.g., ARIMA)
+model_weekly <- auto.arima(time_series_weekly)
+
+plot(model_weekly)
+
+# Generate forecasts
+forecast_values_weekly <- forecast(model_weekly, h = 16)  # Adjust the forecast horizon 'h' as needed
+plot(forecast_values_weekly)
+
+
+
+
+
+# Trabalhar principalmente com sazonalidade -------------------------------
+
+# Tem que fazer um modelo que foque principalmente na sazonalidade e no erro
+
+
+
+
+
+
+
 
